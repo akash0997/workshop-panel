1a642203ea3ec9baac68394aa12017abe4436dae
akash0997 <50735938+akash0997@users.noreply.github.com>
2019-09-27 15:51:23 +0530

    Update module.tsx

--- src/module.tsx
+++ src/module.tsx
@@ -2,28 +2,27 @@ import _ from 'lodash';
 import React, { PureComponent } from 'react';
 import {
   PanelProps,
-  ReactPanelPlugin,
+  PanelPlugin,
   PanelEditorProps,
   PanelOptionsGroup,
   FormField,
   getValueFormat,
-  Gauge,
+  //Gauge,
 } from '@grafana/ui';
 
 export class MyPanel extends PureComponent<PanelProps<MyPanelOptions>> {
   render() {
     const { options } = this.props;
-    const { timeSeries } = this.props.panelData;
+    const { series } = this.props.data;
 
-    const maxValue = _.maxBy(timeSeries[0].datapoints, v => v[0])[0];
+    const maxValue = _.maxBy(series[0].datapoints, v => v[0])[0];
     const formatFunc = getValueFormat('ms');
-    const thresholds = [{ index: 0, value: -Infinity, color: 'green' }, { index: 0, value: 80, color: 'red' }];
+    //const thresholds = [{ index: 0, value: -Infinity, color: 'green' }, { index: 0, value: 80, color: 'red' }];
 
     return (
       <div className="center-vh">
         <h1 style={{ fontSize: '80px' }}>{options.bigText}</h1>
         <h2>Max: {formatFunc(maxValue)}</h2>
-        <Gauge width={200} height={300} unit="ms" value={maxValue} thresholds={thresholds} />
       </div>
     );
   }
@@ -35,7 +34,7 @@ interface MyPanelOptions {
 
 export class MyPanelEditorProps extends PureComponent<PanelEditorProps<MyPanelOptions>> {
   onBigTextChanged = evt => {
-    this.props.onChange({
+    this.props.onOptionsChange({
       ...this.props.options,
       bigText: evt.target.value,
     });
@@ -52,6 +51,6 @@ export class MyPanelEditorProps extends PureComponent<PanelEditorProps<MyPanelOp
   }
 }
 
-export const reactPanel = new ReactPanelPlugin<MyPanelOptions>(MyPanel);
-reactPanel.setEditor(MyPanelEditorProps);
-reactPanel.setDefaults({ bigText: 'GrafanaCon' });
+export const plugin = new PanelPlugin<MyPanelOptions>(MyPanel);
+plugin.setEditor(MyPanelEditorProps);
+plugin.setDefaults({ bigText: 'GrafanaCon' });
